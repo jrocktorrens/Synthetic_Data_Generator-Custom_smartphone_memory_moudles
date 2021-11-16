8dee9a99219ccb05e64e67cf453fc762e813b0f1
Bar.Laniado <barlan iado5@gmail.com>
2021-11-16 16:20:41 +0200

    Add find_node method

--- configuration.py
+++ configuration.py
@@ -1,16 +1,18 @@
 COLOR_MAPS = {'Family': 'cyan', 'Friend': 'red', 'Other': 'green'}
 POSSIBLE_TYPES = ['Family', 'Friend', 'Other']
 TYPE_DIRECT_CONNECTIONS = ['Family', 'Friend', 'Other', None]
-PROB_DIRECT_CONNECTIONS = [0.03, 0.1, 0.37, 0.5]
+PROB_DIRECT_CONNECTIONS = [0.25, 0.25, 0.25, 0.25]
 
 # If a combination not in CONNECTIONS_ROOT_AND_EDGES_OF_OTHER_ROOT the prob is 0
 # {Connection of the current root with the other root, Connection of the other root with the edge}
-CONNECTIONS_ROOT_AND_EDGES_OF_OTHER_ROOT = [{'Family', 'Family'},
+CONNECTIONS_ROOT_AND_EDGES_OF_OTHER_ROOT = [{'Family'},
                                             {'Family', 'Friend'},
                                             {'Family', 'Other'},
-                                            {'Friend', 'Other'}]
-PROB_KIND_OF_CONNECTION = [[0.7, 0.1, 0.05, 0.15],  # { 'Family', 'Family'}
+                                            {'Friend', 'Other'},
+                                            {'Friend'}]
+PROB_KIND_OF_CONNECTION = [[0.15, 0.1, 0.05, 0.7],  # { 'Family', 'Family'}
                            [0.05, 0.3, 0.05, 0.6],  # {'Family', 'Friend'}
-                           [0.05, 0.05, 0.1, 0.8],  # {'Family', 'Friend'}
-                           [0.05, 0.05, 0.1, 0.8]]  # {'Friend', 'Other'}
+                           [0.05, 0.05, 0.1, 0.8],  # {'Family', 'Other'}
+                           [0.05, 0.05, 0.1, 0.8],  # {'Friend', 'Other'}
+                           [0.05, 0.2, 0.1, 0.65]]  # {'Friend', 'Friend'}
 
